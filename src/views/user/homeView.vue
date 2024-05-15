de6d43318a1fea6e1ec5bf927f6da5e24fc6e5f3
zermoser <kittipoj.na@gmail.com>
2024-05-15 14:00:56 +0700

    complete task 7-1

--- src/views/user/homeView.vue
+++ src/views/user/homeView.vue
@@ -1,15 +1,32 @@
 <script setup>
-import userLayout from '@/layouts/userLayout.vue';
-const eiei  = '5555'
+import { onMounted } from 'vue'
+import { useUserProductStore } from '@/stores/user/product.js'
+
+import UserLayout from '@/layouts/userLayout.vue'
+import ProductList from '@/components/productList.vue'
+
+const userProductStore = useUserProductStore()
+
+
+onMounted(() => {
+  userProductStore.loadProduct()
+})
 
 </script>
 
 <template>
-  <div>
-    <userLayout>
-  <h1 class="text-3xl font-bold underline">
-    {{ eiei }}
-  </h1>
-</userLayout>
-</div>
+  <UserLayout>
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Hello to shop</h1>
+          <p class="py-6">สวัสดีจ้า</p>
+        </div>
+      </div>
+    </div>
+    <ProductList
+      :products="userProductStore.list"
+    >
+    </ProductList>
+  </UserLayout>
 </template>
