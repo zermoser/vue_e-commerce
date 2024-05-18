fd6d844db8d8857dd62d3032468d92da657270cb
zermoser <kittipoj.na@gmail.com>
2024-05-18 13:51:02 +0700

    อัปเดตตระกร้าสินค้า

--- src/layouts/userLayout.vue
+++ src/layouts/userLayout.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { RouterLink , useRouter } from 'vue-router'
+import { useUserCartStore } from '@/stores/user/cart'
 
+const userCartStore = useUserCartStore()
 const isLoggedIn = ref(false)
 const router = useRouter()
 const searchText = ref('')
@@ -57,25 +59,25 @@ const logout = () => {
             class="input input-bordered w-24 md:w-auto"
           />
         </div>
-        <div class="dropdown dropdown-end">
-          <label tabindex="0" class="btn btn-ghost btn-circle">
-            <div class="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-              <span class="badge badge-sm indicator-item">0</span>
-            </div>
-          </label>
-          <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-            <div class="card-body">
-              <span class="font-bold text-lg">0 Items</span>
-              <span class="text-info">Subtotal: 100฿</span>
-              <div class="card-actions">
-                <RouterLink to="/cart" class="btn btn-primary btn-block">
-                  View cart
-                </RouterLink>
-              </div>
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost btn-circle">
+          <div class="indicator">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+            <span class="badge badge-sm indicator-item">{{ userCartStore.quantity }}</span>
+          </div>
+        </label>
+        <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+          <div class="card-body">
+            <span class="font-bold text-lg">{{ userCartStore.quantity }} Items</span>
+            <span class="text-info">Subtotal: {{ userCartStore.summaryPrice }} ฿</span>
+            <div class="card-actions">
+              <RouterLink to="/cart" class="btn btn-primary btn-block">
+                View cart
+              </RouterLink>
             </div>
           </div>
         </div>
+      </div>
         <div v-if="!isLoggedIn" class="btn btn-ghost" @click="login">
           Login
         </div>
